1687a91503c05131108fc361f7ca5d337dca677c
Angie Jaramillo <apaola.jaramillo@udea.edu.co>
2023-11-04 13:02:10 -0500

    testing changes

--- Parcial2-qt/juego.cpp
+++ Parcial2-qt/juego.cpp
@@ -10,12 +10,16 @@ jugador *juego::getJugadorActual() const
     return jugadorActual;
 }
 
+tablero *juego::getTableroDeJuego() const
+{
+    return tableroDeJuego;
+}
+
 juego::juego(int tam_tablero, char colorJugador1, char colorJugador2)
 {
     jugador1 = new jugador(colorJugador1);
     jugador2 = new jugador(colorJugador2);
     jugadorActual = jugador1;
-
     tableroDeJuego = new tablero(tam_tablero);
 }
 
@@ -26,18 +30,67 @@ juego::~juego()
     delete tableroDeJuego;
 }
 
-void juego::realizar_movimiento(int fila, int columna)
+bool juego::realizar_movimiento(int fila, int columna)
 {
     if(tableroDeJuego->validar_movimiento(fila,columna,jugadorActual->getColor())){
         tableroDeJuego->mover(fila,columna,jugadorActual->getColor());
+        return true;
     }else{
-        cout<<"El movimiento no está permitido"<<endl;
+        cout<<"El movimiento no esta permitido"<<endl;
+        return false;
     }
 }
 
 void juego::cambiar_turno()
 {
-    jugador* temp = jugador1;
-    jugador1 = jugador2;
-    jugador2 = temp;
+        if (jugadorActual == jugador1) {
+            jugadorActual = jugador2;
+        } else {
+            jugadorActual = jugador1;
+        }
+}
+
+
+
+bool juego::vrf_fin_juego()
+{
+    if (tableroDeJuego->tablero_lleno()){
+        return true;
+    }
+
+    bool no_mov_1 = !tableroDeJuego->vrf_mov_disp(jugador1->getColor());
+    bool no_mov_2 = !tableroDeJuego->vrf_mov_disp(jugador2->getColor());
+
+    if (no_mov_1 && no_mov_2) {
+        return true;
+    }
+    return false;
+}
+
+char juego::vrf_ganador(int &fichas_ganador)
+{
+    int fichas_1=tableroDeJuego->contar_fichas(jugador1->getColor());
+    int fichas_2=tableroDeJuego->contar_fichas(jugador2->getColor());
+
+    if (fichas_1 > fichas_2) {
+        fichas_ganador=fichas_1;
+        return jugador1->getColor();
+    }
+    else if (fichas_2 > fichas_1) {
+        fichas_ganador=fichas_2;
+        return jugador2->getColor();
+    }
+    else {
+        fichas_ganador=fichas_1;
+        return 'e'; // Empate
+    }
+
+}
+
+bool juego::pre_cambiar_turno()
+{
+    if(tableroDeJuego->vrf_mov_disp(getJugadorActual()->getColor())){
+        return false;
+    }
+    return true;
 }
